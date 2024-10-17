dcd18f8da604116535a113d959f9e09da2fdc6e8
Saeedullah15 <saeedullahazim65@gmail.com>
2024-10-17 22:23:35 +0600

    displayed list of books, added some styles and responsiveness

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
-function loadData() {
+function loadBookData() {
     fetch("https://gutendex.com/books")
         .then(res => res.json())
-        .then(data => console.log(data.results))
+        .then(data => displayBookData(data.results))
 }
-loadData();
+
+function displayBookData(bookData) {
+    console.log(bookData);
+    const booksContainer = document.getElementById("booksContainer");
+
+    bookData.forEach(book => {
+        const bookDiv = document.createElement("div");
+        bookDiv.classList.add("book");
+
+        // Create a list of genres
+        const genres = book.subjects;
+        let genreList = '<ul>';
+        genres.forEach(genre => {
+            genreList += `<li>${genre}</li>`;
+        });
+        genreList += '</ul>';
+
+        bookDiv.innerHTML = `
+            <img src=${book.formats['image/jpeg']} alt="cover image">
+            <h2>Title: ${book.title}</h2>
+            <p>Authors Name: ${book.authors.map(author => author.name)}</p>
+            <hr>
+            <p>Genre: ${genreList}</p>
+            <hr>
+            <p>Id: ${book.id}</p>
+        `;
+
+        booksContainer.appendChild(bookDiv);
+    })
+}
+
+loadBookData();
